c2cd667331417e09ff21f4f676c5dc3063742b16
mehmetresitcagan@posta.mu.edu.tr <mehmetresitcagan@posta.mu.edu.tr>
2023-08-08 16:47:48 +0300

    Day 2

---
+++ demo/src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @GetMapping(value = "/")
+    public boolean check() {
+        return true;
+    }
+
+    @GetMapping(value = "/murat/")
+    public String gayMi() {
+        return "gayMurat";
+    }
+}
